238b8f25b130355ea40f5245b3e6b2057e0fb7b2
pizht <chaoxichun@outlook.com>
2026-06-24 23:15:45 +0800

    fixup! refactor(config): 提取配置文件到独立config目录

--- src/domain/scene/factories.ts
+++ src/domain/scene/factories.ts
@@ -1,4 +1,4 @@
-import { DEFAULT_FONT_FAMILY } from '@/app/lib/fonts'
+import { DEFAULT_FONT_FAMILY } from '@/config/fonts'
 import type { TextElement, ShapeElement, ImageElement } from './types'
 
 export function createTextElement(): TextElement {
